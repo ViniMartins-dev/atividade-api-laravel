49a192804dd7e6dc760404637b7da81ede6aac93
vinicius <vinicius_apple22@outlook.com>
2025-02-28 22:27:38 -0300

    commit

--- app/Http/Controllers/usuario.php
+++ app/Http/Controllers/usuario.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class usuario extends Controller
 {
-    //
+  /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(manchete $manchete)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, manchete $manchete)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(manchete $manchete)
+    {
+        //
+    }
 }
